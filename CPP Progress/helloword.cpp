6581a3fc4b25c7b214c6bffeeae3ea86e8472c0a
Eswar sai Yarra <119247902+eswarsaiyarra2602@users.noreply.github.com>
2023-10-31 00:27:44 +0530

    Update helloword.cpp

--- CPP Progress/helloword.cpp
+++ CPP Progress/helloword.cpp
@@ -1,1 +1,13 @@
-
+#include<iostream>
+#include<string>
+using namespace std;
+class fun{
+  string msg="Hello World!";
+  public:
+  void greet(){ cout<<msg<<endl;}
+};
+int main()
+{
+  fun myObj1;  
+   myObj1.greet();
+}
