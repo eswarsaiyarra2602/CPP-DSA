421ec3a5326a5cb12c3c07079b31717af15cc299
Eswar sai Yarra <119247902+eswarsaiyarra2602@users.noreply.github.com>
2023-10-31 00:32:50 +0530

    Update rotate.cpp

--- CPP Progress/rotate.cpp
+++ CPP Progress/rotate.cpp
@@ -1,1 +1,14 @@
-
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    int data[]={11,21,34,53,123};
+    rotate(data,data+2, data+5);
+    for(int i=0 ; i<5;i++)
+    {
+        cout<<data[i]<<" ";
+    }
+    return 0;
+}
+//roate desired set of elements - basically brings that set of elements to the begging of the array
