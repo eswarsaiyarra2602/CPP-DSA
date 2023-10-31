55d24f69c269b74f02f181fc92c5a500366a2d2f
Eswar sai Yarra <119247902+eswarsaiyarra2602@users.noreply.github.com>
2023-10-31 08:32:33 +0530

    Update validparanthesis.cpp

--- DSA Progress/Stack/validparanthesis.cpp
+++ DSA Progress/Stack/validparanthesis.cpp
@@ -1,1 +1,29 @@
-
+//leetcode-20
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(auto c:s)
+        {
+            if(c=='(' ||  c=='{' || c=='[')
+            {
+                st.push(c);
+            }
+            else
+            {
+                if(st.empty()||
+                (c==')' && st.top()!='(')||
+                (c=='}' && st.top()!='{')||
+                (c==']' && st.top()!='['))
+                {
+                    return false;
+                }
+                else{
+                    st.pop();
+                }
+            }
+        }
+        if(st.empty()){return true;}
+        else{return false;}
+    }
+};
