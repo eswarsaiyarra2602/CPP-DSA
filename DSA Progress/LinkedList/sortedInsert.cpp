dd14b379fda0c2a1ae5a01823417057e8680e259
Eswar sai Yarra <119247902+eswarsaiyarra2602@users.noreply.github.com>
2023-12-19 22:32:10 +0530

    Create sortedInsert.cpp

---
+++ DSA Progress/LinkedList/sortedInsert.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+struct Node{
+    int data ; 
+    Node* next = nullptr;
+};
+void SortedInsert(Node*&head , int num)
+{
+    Node* node = new Node ; 
+    node->data = num ;
+    
+    //insertatbegging 
+    if(head==nullptr || head->data>num)
+    {
+        node->next=head;
+        head=node;
+    }
+    else{
+        Node* currentNode = head ;
+        while(currentNode->next!=nullptr && currentNode->next->data<num)
+        {
+            currentNode=currentNode->next;
+        }
+        Node* temp = currentNode ->next;
+        currentNode->next = node;
+        node->next = temp;
+    }
+}
+void display(Node*&head)
+{
+    Node* currentNode = head ;
+    while(currentNode!=nullptr)
+    {
+        cout<<currentNode->data<<" ";
+        currentNode=currentNode->next;
+    }
+}
+int main()
+{
+    Node*head = nullptr;
+    int n , num , x;
+    cin>>n;
+    while(n--)
+    {
+        cin>>num;
+        SortedInsert(head,num);
+    }
+    cin>>x;
+    SortedInsert(head,x);
+    display(head);
+}
